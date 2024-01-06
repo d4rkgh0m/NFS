128f8f30eb4c3b112cf727888c5540bfc19b1bc2
Pavel <120186195+d4rkgh0m@users.noreply.github.com>
2024-01-06 16:36:51 +0300

    Add files via upload

---
+++ nfss_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo yum install nfs-utils -y
+sudo systemctl enable firewalld --now
+sudo firewall-cmd --add-service="nfs3" --add-service="rpc-bind" --add-service="mountd" --permanent
+sudo firewall-cmd --reload
+sudo systemctl enable nfs --now
+sudo mkdir -p /srv/share/upload
+sudo chown -R nfsnobody:nfsnobody /srv/share
+sudo chmod 0777 /srv/share/upload
+sudo cat << EOF > /etc/exports
+/srv/share 192.168.56.10/24(rw,sync,root_squash)
+EOF
+sudo exportfs -r
+sudo touch /srv/share/upload/client_file
