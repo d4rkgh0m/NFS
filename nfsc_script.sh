a119b0df7d9fb421ca3998b4dcf5a3a51accdab7
Pavel <120186195+d4rkgh0m@users.noreply.github.com>
2024-01-15 00:42:28 +0300

    nfsc_script.sh

--- nfsc_script.sh
+++ nfsc_script.sh
@@ -2,7 +2,7 @@
 
 sudo yum install nfs-utils -y
 sudo systemctl enable firewalld --now
-sudo systemctl status firewalld
 sudo echo "192.168.56.10:/srv/share/ /mnt nfs vers=3,proto=udp,noauto,x-systemd.automount 0 0" >> /etc/fstab
 sudo systemctl daemon-reload
 sudo systemctl restart remote-fs.target
+sudo touch /mnt/upload/client_file
